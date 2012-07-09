21d7a355257913ccb37e2a5addacd141159e07f9
Richard Nichols <rn@visural.com>
2012-07-09 22:48:09 +1000

    Switch to maven

---
+++ src/main/java/com/visural/common/cache/CacheSettings.java
@@ -0,0 +1,41 @@
+package com.visural.common.cache;
+
+import java.io.Serializable;
+
+public class CacheSettings implements Serializable {
+
+    private int timeToLive;
+    private int maxEntries;
+    private EvictionStrategy evictionStrategy;   
+    private boolean softValues;   
+    private boolean singletonCache;
+
+    public CacheSettings(Cache settings) {
+        timeToLive = settings.timeToLive();
+        maxEntries = settings.maxEntries();
+        evictionStrategy = settings.evictionStrategy();
+        softValues = settings.softValues();
+        singletonCache = settings.singletonCache();
+    }
+
+    public EvictionStrategy getEvictionStrategy() {
+        return evictionStrategy;
+    }
+
+    public int getMaxEntries() {
+        return maxEntries;
+    }
+
+    public int getTimeToLive() {
+        return timeToLive;
+    }
+
+    public boolean isSingletonCache() {
+        return singletonCache;
+    }
+
+    public boolean isSoftValues() {
+        return softValues;
+    }
+    
+}
